8f2da7a87dc145011cc7b81439300671ad55e45f
horisanLog <ttie7374@gmail.com>
2022-12-01 16:52:27 +0900

    バックエンド側のfirestoreの追加

--- backend/app/controllers/api/hello_controller.rb
+++ backend/app/controllers/api/hello_controller.rb
@@ -1,5 +1,20 @@
 class Api::HelloController < ApplicationController
+  require 'google/cloud/firestore'
+
   def index
     render json: { id: 1, name: "リスト" } , status: :ok
+  end
+
+  def create
+    save_firestore
+    head :no_content
+  end
+
+  private
+
+  def save_firestore
+    firestore = Google::Cloud::Firestore.new(project_id: 'park-platform', credentials: File.new('park-platform-firebase-adminsdk-x8tzh-9cebe8cf2b.json').to_path)
+    doc_ref = firestore.doc("todos/1") # 保存先のパスを指定
+    doc_ref.set({email: "test@test.com", text: "test", title: "backend"}) # 値を書き込む
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   namespace :api do
-    resources :hello, only: [:index]
+    resources :hello, only: [:index, :create]
   end
 end
