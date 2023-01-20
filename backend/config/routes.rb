e2fa2400073c27e727f3728bcb21e1907c86e2af
horisanLog <ttie7374@gmail.com>
2023-01-20 14:22:02 +0900

    graphiqlのセットアップ

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: 'graphql#execute' if Rails.env.development?
+  post "/graphql", to: "graphql#execute"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
