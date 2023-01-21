aa0241cfda070e3b21fb5be9f108c05c7c58d7e1
horisanLog <ttie7374@gmail.com>
2023-01-21 12:02:49 +0900

    apollo/clientの設定

---
+++ frontend/graphql/index.ts
@@ -0,0 +1,8 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: `${process.env.NEXT_PUBLIC_API_ENDPOINT}/graphql`,
+  cache: new InMemoryCache(),
+})
+
+export default client
